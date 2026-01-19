0de62e96c5989ef56b3a86e36cc954313c3cc6e8
irihojaphet <irihojaja@yahoo.com>
2026-01-19 15:59:50 +0200

    Fix Prisma migration 20260102004207_backup to avoid duplicate idempotencyKey changes

--- backend/prisma/migrations/20260102004207_backup/migration.sql
+++ backend/prisma/migrations/20260102004207_backup/migration.sql
@@ -4,8 +4,6 @@
   - You are about to drop the column `googleId` on the `User` table. All the data in the column will be lost.
   - You are about to drop the column `provider` on the `User` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[userId,referredUserId]` on the table `AffiliateEarning` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[idempotencyKey]` on the table `InternalTransfer` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[idempotencyKey]` on the table `Transaction` will be added. If there are existing duplicate values, this will fail.
 
 */
 -- DropIndex
@@ -15,17 +13,13 @@ DROP INDEX "User_googleId_key";
 ALTER TABLE "Bet" ADD COLUMN     "isDemo" BOOLEAN NOT NULL DEFAULT false;
 
 -- AlterTable
-ALTER TABLE "InternalTransfer" ADD COLUMN     "idempotencyKey" TEXT;
-
--- AlterTable
 ALTER TABLE "PremiumSubscription" ADD COLUMN     "isSimulated" BOOLEAN NOT NULL DEFAULT false;
 
 -- AlterTable
 ALTER TABLE "Spin" ADD COLUMN     "isDemo" BOOLEAN NOT NULL DEFAULT false;
 
 -- AlterTable
-ALTER TABLE "Transaction" ADD COLUMN     "idempotencyKey" TEXT,
-ADD COLUMN     "isDemo" BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE "Transaction" ADD COLUMN     "isDemo" BOOLEAN NOT NULL DEFAULT false;
 
 -- AlterTable
 ALTER TABLE "User" DROP COLUMN "googleId",
@@ -50,7 +44,4 @@ CREATE INDEX "AffiliateEarning_date_idx" ON "AffiliateEarning"("date");
 CREATE UNIQUE INDEX "AffiliateEarning_userId_referredUserId_key" ON "AffiliateEarning"("userId", "referredUserId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "InternalTransfer_idempotencyKey_key" ON "InternalTransfer"("idempotencyKey");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Transaction_idempotencyKey_key" ON "Transaction"("idempotencyKey");
+-- Note: idempotencyKey columns and unique indexes are introduced in migration `20251227194454_`.
